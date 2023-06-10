de0f5d3664b7afa6901676b12018f899c0ad64e8
Le-TruongUyPhu <Le.TruongUyPhu@coxautoinc.com>
2023-06-10 16:02:36 +0700

    Create new project
    Create new project

---
+++ src/com/ltu/Solutions.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.ltu;
+
+/**
+ * @author FPT LA
+ *
+ */
+public class Solutions {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Test");
+	}
+
+}
---
+++ test/com/ltu/TestSolutions.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.ltu;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author FPT LA
+ *
+ */
+class TestSolutions {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
